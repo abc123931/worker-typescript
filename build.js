12fea2f8161e6b5428dce63e5376d2e9c7d072cc
abc123931 <abc123931@gmail.com>
2021-12-01 21:42:54 +0900

    ♻️ Fix indent.

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const { build } = require("esbuild");
 
 build({
-	entryPoints: ["./src/index.ts"],
-	bundle: true,
+  entryPoints: ["./src/index.ts"],
+  bundle: true,
   platform: "node",
-	outfile: "./dist/worker.js",
+  outfile: "./dist/worker.js",
   bundle: true,
   minify: true,
 }).catch((err) => console.log(`Error: ${JSON.stringify(err)}`));
