f6586308eb30318706b3afa0cbbcf9cd9a8ba4a0
abc123931 <abc123931@gmail.com>
2021-12-02 12:02:16 +0900

    🔧 Add webpack.

--- build.js
+++ build.js
@@ -1,10 +1,15 @@
 const { build } = require("esbuild");
+const alias = require("esbuild-plugin-alias");
 
 build({
   entryPoints: ["./src/index.ts"],
   bundle: true,
   platform: "node",
   outfile: "./dist/worker.js",
-  bundle: true,
-  minify: true,
+  minify: false,
+  plugins: [
+    alias({
+      "@prisma/client$": require.resolve("@prisma/client"),
+    }),
+  ],
 }).catch((err) => console.log(`Error: ${JSON.stringify(err)}`));
--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,13 @@
-export const handleRequest = async (request: Request): Promise<Response> => {
-  return new Response(`request method: ${request.method}`);
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const handleRequest = async (): Promise<Response> => {
+  try {
+    const test = await prisma.test.findFirst();
+    return new Response(`test first id: ${test?.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+  return new Response(`test first id: error`);
 };
